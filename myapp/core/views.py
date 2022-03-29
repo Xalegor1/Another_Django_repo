d54d413524d7d6ff5ad9258374ea993cb2336cb9
Xalegor <Argus1672@gmail.com>
2022-03-29 21:29:10 +0500

    second_change

--- myapp/core/views.py
+++ myapp/core/views.py
@@ -1,6 +1,14 @@
+import re
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse('Hello world!')
+    context = [
+        {'breath': 'Breath of the Sun'},
+        {'breath': 'Water style: Shark breath'}
+    ]
+    return render(request, 'core/index.html', {
+        'show_breath': True,
+        'breaths': context
+    })
